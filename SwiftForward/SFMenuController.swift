de79ce253b1896f991580ad95a490364617d9198
Zhouhaijian <viczy@ymail.com>
2015-03-12 11:20:57 +0800

    project init

---
+++ SwiftForward/SFMenuController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwiftForward
+//
+//  Created by Vic Zhou on 3/11/15.
+//  Copyright (c) 2015 everycode. All rights reserved.
+//
+
+import UIKit
+
+class SFMenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
